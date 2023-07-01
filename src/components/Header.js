fe3da7fc94df5f7f433b95c65cae91dc72316c67
Natalya Dmitrieva <buzlk.n@yandex.ru>
2023-07-01 12:05:39 +0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import headerLogo from './../images/gif-svgrepo-com.svg';
 
 const Header = () => {
   return (
-    <div>LOGO</div>
-  )
-}
+    <header className="header">
+      <img src={headerLogo} alt="Логотип" className="header__logo" />
+      <div className="header__navbar">
+        <Link to="/" className="header__link">
+          Поиск
+        </Link>
+        <Link to="/trends" className="header__link">
+          Тренды
+        </Link>
+        <Link to="/random" className="header__link">
+          Случайный гиф
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
