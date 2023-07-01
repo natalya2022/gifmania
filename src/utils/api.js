62c293d83c618909dc0584c6943f070b15561247
Daniil Kopylov <Shinoinochi@yandex.ru>
2023-07-01 19:56:57 +0500

    создание api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,28 @@
 export default class Api {
     constructor({baseURL, key}) {
         this._URL = baseURL;
-        this.key = key;
+        this._key = key;
     }
-    search() {
-        return fetch(`api.giphy.com/v1/gifs/trending?api_key=RdGZuc1yz5OxCXh5U86bmFqDT03Tw7ij`, {
+    trending() {
+        return fetch(`${this._URL}trending?api_key=${this._key}`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+          })
+          .then((res) => {
+            return res.ok? res.json(): Promise.reject(`Ошибка: ${res.status}`);
+          });
+    }
+    search(word) {
+        return fetch(`${this._URL}search?api_key=${this._key}&q=${word}`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+          })
+          .then((res) => {
+            return res.ok? res.json(): Promise.reject(`Ошибка: ${res.status}`);
+          });
+    }
+    random() {
+        return fetch(`${this._URL}random?api_key=${this._key}`, {
             method: 'GET',
             headers: {'Content-Type': 'application/json'}
           })
@@ -14,11 +32,7 @@ export default class Api {
     }
 }
 
-
-const api = new Api({
-    baseURL: "api.giphy.com/v1/gifs/",
+export const api = new Api({
+    baseURL: "https://api.giphy.com/v1/gifs/",
     key: "RdGZuc1yz5OxCXh5U86bmFqDT03Tw7ij"
-});
-api.search().then(res => {
-    console.log(res);
 });
