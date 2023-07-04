3a2edf6c44fa67dc93a319057c50f52ffdfc6264
Natalya Dmitrieva <buzlk.n@yandex.ru>
2023-07-04 16:25:27 +0400

    светлая тема окончательная

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,38 @@ import headerLogoDark from './../images/logo22.png';
 const Header = ({ onTrendClick, onRandomClick, onSearchClick, onChangeTheme, isTheme }) => {
   return (
     <header className="header">
-      <img src={isTheme? `${headerLogoLight}` : `${headerLogoDark}`} alt="Логотип" className="header__logo" />
-      <div className={isTheme? `header__navbar header__navbar_light-theme` : `header__navbar`}>        
-        <NavLink to="/" className={isTheme? `header__link header__link_light-theme` : `header__link`} onClick={onSearchClick}>
+      <label
+        className={isTheme ? `button-theme button-theme_active` : `button-theme`}
+        onClick={onChangeTheme}
+      />
+      <img
+        src={isTheme ? `${headerLogoLight}` : `${headerLogoDark}`}
+        alt="Логотип"
+        className="header__logo"
+      />
+      <div className={isTheme ? `header__navbar header__navbar_light-theme` : `header__navbar`}>
+        <NavLink
+          to="/"
+          className={isTheme ? `header__link header__link_light-theme` : `header__link`}
+          onClick={onSearchClick}
+        >
           Поиск
         </NavLink>
-        <NavLink to="/trends" className={isTheme? `header__link header__link_light-theme` : `header__link`} onClick={onTrendClick}>
+        <NavLink
+          to="/trends"
+          className={isTheme ? `header__link header__link_light-theme` : `header__link`}
+          onClick={onTrendClick}
+        >
           Тренды
         </NavLink>
-        <NavLink to="/random" className={isTheme? `header__link header__link_light-theme` : `header__link`} onClick={onRandomClick}>
+        <NavLink
+          to="/random"
+          className={isTheme ? `header__link header__link_light-theme` : `header__link`}
+          onClick={onRandomClick}
+        >
           Случайный гиф
         </NavLink>
-        <label className={isTheme? `button-theme button-theme_active` : `button-theme`} onClick={onChangeTheme}/>
       </div>
-        
     </header>
   );
 };
